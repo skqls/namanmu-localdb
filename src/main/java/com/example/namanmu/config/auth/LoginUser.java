5a8af5d2352b5186ac948950fac186a6e24654e1
skqls <kitchin105@gmail.com>
2023-01-05 23:25:14 +0900

    first

---
+++ src/main/java/com/example/namanmu/config/auth/LoginUser.java
@@ -0,0 +1,15 @@
+package com.example.namanmu.config.auth;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
+
